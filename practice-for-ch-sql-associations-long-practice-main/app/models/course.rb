7f4d4c9db3f74dd8501ce0325286a0d2afc46387
Arvid Hossain <arvidh97@gmail.com>
2023-04-06 16:36:11 -0400

    finished the project

--- practice-for-ch-sql-associations-long-practice-main/app/models/course.rb
+++ practice-for-ch-sql-associations-long-practice-main/app/models/course.rb
@@ -10,6 +10,9 @@
 #  updated_at    :datetime         not null
 #
 class Course < ApplicationRecord
+    
+
+
     has_many :enrollments,
         primary_key: :id,
         foreign_key: :course_id,
@@ -18,5 +21,16 @@ class Course < ApplicationRecord
     has_many :enrolled_students,
         through: :enrollments,
         source: :student
+
+    belongs_to :prerequisite,
+        primary_key: :id, 
+        foreign_key: :prereq_id,
+        class_name: :Course,
+        optional: true
+
+    belongs_to :instructor,
+        primary_key: :id,
+        foreign_key: :instructor_id,
+        class_name: :User
 
 end
--- practice-for-ch-sql-associations-long-practice-main/app/models/user.rb
+++ practice-for-ch-sql-associations-long-practice-main/app/models/user.rb
@@ -16,4 +16,10 @@ class User < ApplicationRecord
     has_many :enrolled_courses,
         through: :enrollments,
         source: :courses
+
+    has_many :teach_course,
+        primary_key: :id,
+        foreign_key: :instructor_id,
+        class_name: :Course
+    
 end
